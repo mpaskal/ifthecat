ba1011c8dfb7f88f4ae2e52b570cac6f414e2c9a
Marina Paskal <paskalm@rogers.com>
2021-09-14 02:14:13 -0400

    Home page basic

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+var msgIX = 0;
+var msgs = new Array(
+  "Cat's Human",
+  "Cat's Lover",
+  "Veterinarian",
+  "Business for Cats and their Humans"
+);
+
+function displayMessage(milliseconds) {
+  if (msgIX < msgs.length) {
+    var heading = document.getElementById("catWatchers");
+    heading.firstChild.nodeValue = msgs[msgIX];
+    msgIX++;
+    window.setTimeout(function () {
+      displayMessage(milliseconds);
+    }, milliseconds);
+    if (msgIX >= msgs.length) {
+      msgIX = 0;
+    }
+  }
+}
